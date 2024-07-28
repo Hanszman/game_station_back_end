cc4d66dffcd4fb38bbd97094806579bbb03e5d2d
Hanszman <victor.hanszman@hotmail.com>
2024-07-28 16:48:39 -0300

    fixes on port

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import { connectToDatabase } from './database/db';
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const host = process.env.HOST || 'http://localhost';
 
 app.get('/', async (req, res) => {
