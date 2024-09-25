b74916c8ccbb687ea9dc4b3302f841cf9efc6a40
Hanszman <victor.hanszman@hotmail.com>
2024-09-25 10:31:52 -0300

    continuing crud

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,35 @@
 import { Request, Response } from 'express';
 import UserModel from '../models/userModel';
 
-const userRead = async (req: Request, res: Response): Promise<void> => {
-    res.json('userRead');
+const readUser = async (req: Request, res: Response): Promise<void> => {
+    let result: any;
+    let status: number = 200;
+    let error: boolean = false;
+    const id = req?.params?.id;
+    const queryParams = req?.query;
+    if (id) {
+        result = await UserModel.findByPk(id);
+    } else {
+        result = await UserModel.findAll();
+    }
+    res.status(status).json({error, data: result});
+}
+
+const createUser = async (req: Request, res: Response): Promise<void> => {
+    res.json('readUser');
+}
+
+const updateUser = async (req: Request, res: Response): Promise<void> => {
+    res.json('readUser');
+}
+
+const deleteUser = async (req: Request, res: Response): Promise<void> => {
+    res.json('readUser');
 }
 
 export default {
-    userRead
+    readUser,
+    createUser,
+    updateUser,
+    deleteUser
 }
--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -5,22 +5,26 @@ import userController from '../controllers/userController';
 const router = express.Router();
 
 // GET
-router.get('/', (req: Request, res: Response) => res.send('Hello World'));
-router.get('/user', userController.userRead);
+router.get('/', (req: Request, res: Response) => res.send('Welcome to Game Station API! Access a valid endpoint route in the URL'));
+router.get('/user', userController.readUser);
+router.get('/user/:id', userController.readUser);
 router.get('/db', async (req: Request, res: Response) => {
     try {
         const [results, metadata] = await sequelize.query('SELECT 1 + 1 AS solution');
-        res.status(200).json({results, metadata});
+        res.status(200).json({data: {results, metadata}});
     } catch (error: any) {
         res.status(500).send(error.message);
     }
 });
 
 // POST
-router.get('/login', loginController.loginAuth);
+router.post('/login', loginController.loginAuth);
+router.post('/user', userController.createUser);
 
 // PUT
+router.put('/user', userController.updateUser);
 
 // DELETE
+router.delete('/user', userController.deleteUser);
 
 export default router;
