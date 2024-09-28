ad67ca0f6de9b04089d1407fdc34257847d06613
Hanszman <victor.hanszman@hotmail.com>
2024-09-28 14:29:28 -0300

    first orm crud try

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -6,7 +6,7 @@ import QueryOptions from '../utils/classes/QueryOptions';
 const readUser = async (req: Request, res: Response): Promise<void> => {
     return handleEndpoint(req, res, async (req) => {
         const id = req?.params?.id;
-        const queryParams = req?.query;
+        // const queryParams = req?.query; // TODO: utilizar para filtrar e ordenar dados, além de fazer joins das tabelas
         const queryOptions = new QueryOptions<UserModel>();
         console.log('queryOptions', queryOptions);
         if (id) {
@@ -18,15 +18,48 @@ const readUser = async (req: Request, res: Response): Promise<void> => {
 }
 
 const createUser = async (req: Request, res: Response): Promise<void> => {
-    res.json('readUser');
+    return handleEndpoint(req, res, async (req) => {
+        const bodyData = req?.body;
+        if (bodyData) {
+            return await UserModel.create(bodyData);
+        } else {
+            return null;
+        }
+    });
 }
 
 const updateUser = async (req: Request, res: Response): Promise<void> => {
-    res.json('readUser');
+    return handleEndpoint(req, res, async (req) => {
+        const id = req?.params?.id;
+        const bodyData = req?.body;
+        if (id && bodyData) {
+            const user = await UserModel.findByPk(id);
+            if (user) {
+                Object.assign(user, bodyData);
+                return await user.save();
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    });
 }
 
 const deleteUser = async (req: Request, res: Response): Promise<void> => {
-    res.json('readUser');
+    return handleEndpoint(req, res, async (req) => {
+        const id = req?.params?.id;
+        if (id) {
+            const user = await UserModel.findByPk(id);
+            if (user) {
+                return await user.destroy();
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    });
 }
 
 export default {
