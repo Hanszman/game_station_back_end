5386c6ec676d5171adbcb80836cfbf869480517e
Hanszman <victor.hanszman@hotmail.com>
2024-10-01 09:04:17 -0300

    implementing login and bcrypt

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,8 +1,22 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import handleEndpoint from '../utils/handleEndpoint';
 import UserModel from '../models/userModel';
 
 const loginAuth = async (req: Request, res: Response): Promise<void> => {
-    res.json('loginAuth');
+    return handleEndpoint(req, res, async (req) => {
+        const bodyData = req?.body;
+        if (bodyData && bodyData?.username && bodyData?.password) {
+            const user = await UserModel.findOne({ where: { username: bodyData.username } });
+            if (user && bcrypt.compareSync(bodyData.password, user.password)) {
+                return user;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
+    });
 }
 
 export default {
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
 import handleEndpoint from '../utils/handleEndpoint';
 import UserModel from '../models/userModel';
-// import QueryOptions from '../utils/classes/QueryOptions';
 
 const readUser = async (req: Request, res: Response): Promise<void> => {
     return handleEndpoint(req, res, async (req) => {
         const id = req?.params?.id;
-        // const queryParams = req?.query; // TODO: utilizar para filtrar e ordenar dados, além de fazer joins das tabelas
-        // const queryOptions = new QueryOptions<UserModel>();
         if (id) {
             return await UserModel.findByPk(id);
         } else {
@@ -20,6 +18,10 @@ const createUser = async (req: Request, res: Response): Promise<void> => {
     return handleEndpoint(req, res, async (req) => {
         const bodyData = req?.body;
         if (bodyData) {
+            const password = bodyData?.password ?? '';
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
+            bodyData.password = hash;
             return await UserModel.create(bodyData);
         } else {
             return null;
@@ -33,7 +35,12 @@ const updateUser = async (req: Request, res: Response): Promise<void> => {
         const bodyData = req?.body;
         if (id && bodyData) {
             const user = await UserModel.findByPk(id);
-            if (user) {
+            if (user && user.password === bodyData?.oldPassword) {
+                const password = bodyData?.password ?? '';
+                const salt = bcrypt.genSaltSync(10);
+                const hash = bcrypt.hashSync(password, salt);
+                bodyData.password = hash;
+                delete bodyData.oldPassword;
                 Object.assign(user, bodyData);
                 return await user.save();
             } else {
@@ -49,6 +56,7 @@ const deleteUser = async (req: Request, res: Response): Promise<void> => {
     return handleEndpoint(req, res, async (req) => {
         const id = req?.params?.id;
         if (id) {
+            // TODO: CASCADE: delete all user's in other tables
             const user = await UserModel.findByPk(id);
             if (user) {
                 return await user.destroy();
