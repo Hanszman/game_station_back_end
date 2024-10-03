353d7cf1cfe01072d427d0bfc7f7f9d4292ee678
Hanszman <victor.hanszman@hotmail.com>
2024-10-03 10:12:08 -0300

    osr-sp129: creating userController testing crud

---
+++ src/tests/userController.test.ts
@@ -0,0 +1,164 @@
+import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
+import userController from '../controllers/userController';
+import UserModel from '../models/userModel';
+
+jest.mock('../models/userModel');
+jest.mock('bcryptjs');
+
+describe('User Controller', () => {
+    let req: Partial<Request>;
+    let res: Partial<Response>;
+    let jsonMock: jest.Mock;
+    let statusMock: jest.Mock;
+    beforeEach(() => {
+        jsonMock = jest.fn();
+        statusMock = jest.fn().mockReturnThis();
+        req = { params: {}, body: {} };
+        res = { json: jsonMock, status: statusMock };
+    });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+    describe('readUser', () => {
+        it('deve retornar todos os usuários', async () => {
+            const users = [{ id: 1, name: 'User1' }];
+            (UserModel.findAll as jest.Mock).mockResolvedValue(users);
+            await userController.readUser(req as Request, res as Response);
+            expect(UserModel.findAll).toHaveBeenCalled();
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: users,
+                error: false,
+                message: 'Success!',
+            });
+        });
+        it('deve retornar um usuário por ID', async () => {
+            const user = { id: 1, name: 'User1' };
+            req.params = { id: '1' };
+            (UserModel.findByPk as jest.Mock).mockResolvedValue(user);
+            await userController.readUser(req as Request, res as Response);
+            expect(UserModel.findByPk).toHaveBeenCalledWith('1');
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: user,
+                error: false,
+                message: 'Success!',
+            });
+        });
+    });
+    describe('createUser', () => {
+        it('deve criar um novo usuário com hash de senha', async () => {
+            const user = {
+                id: 1,
+                name: 'User1',
+                password: 'hashedPassword',
+            };
+            req.body = {
+                name: 'User1',
+                password: 'plainPassword',
+            };
+            const hashedPassword = 'hashedPassword';
+            (UserModel.create as jest.Mock).mockResolvedValue(user);
+            (bcrypt.hashSync as jest.Mock).mockReturnValue(hashedPassword);
+            await userController.createUser(req as Request, res as Response);
+            expect(UserModel.create).toHaveBeenCalledWith({
+                ...req.body,
+                password: hashedPassword,
+            });
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: user,
+                error: false,
+                message: 'Success!',
+            });
+        });
+    });
+    describe('updateUser', () => {
+        it('deve atualizar um usuário com senha correta', async () => {
+            const user = {
+                id: 1,
+                username: 'user1',
+                name: 'User1',
+                password: 'oldPassword',
+                email: 'user1@example.com',
+                save: jest.fn().mockResolvedValue({
+                    id: 1,
+                    username: 'user1',
+                    name: 'User1',
+                    email: 'user1@example.com',
+                    password: 'newHashedPassword',
+                }),
+                _attributes: {},
+                dataValues: {},
+            } as unknown as UserModel;
+            req.params = { id: '1' };
+            req.body = { oldPassword: 'oldPassword', password: 'newPassword' };
+            (UserModel.findByPk as jest.Mock).mockResolvedValue(user);
+            const salt = 'randomSalt';
+            const hashedPassword = 'newHashedPassword';
+            (bcrypt.genSaltSync as jest.Mock).mockReturnValue(salt);
+            (bcrypt.hashSync as jest.Mock).mockReturnValue(hashedPassword);
+            await userController.updateUser(req as Request, res as Response);
+            const expectedUser = {
+                id: 1,
+                username: 'user1',
+                name: 'User1',
+                email: 'user1@example.com',
+                password: 'newHashedPassword',
+            };
+            expect(UserModel.findByPk).toHaveBeenCalledWith('1');
+            expect(bcrypt.hashSync).toHaveBeenCalledWith('newPassword', salt);
+            expect(user.save).toHaveBeenCalled();
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: expectedUser,
+                error: false,
+                message: 'Success!',
+            });
+        });
+        it('deve retornar null se o usuário ou a senha antiga estiver incorreta', async () => {
+            const user = {
+                id: 1,
+                username: 'user1',
+                name: 'User1',
+                password: 'differentPassword',
+                email: 'user1@example.com',
+                _attributes: {},
+                dataValues: {},
+            } as unknown as UserModel;
+            req.params = { id: '1' };
+            req.body = { oldPassword: 'oldPassword', password: 'newPassword' };
+            (UserModel.findByPk as jest.Mock).mockResolvedValue(user);
+            await userController.updateUser(req as Request, res as Response);
+            expect(UserModel.findByPk).toHaveBeenCalledWith('1');
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: null,
+                error: true,
+                message: 'Bad Request!',
+            });
+        });
+    });
+    describe('deleteUser', () => {
+        it('deve deletar um usuário pelo ID', async () => {
+            const user = { id: 1, name: 'User1', destroy: jest.fn().mockResolvedValue(true) };
+            req.params = { id: '1' };
+            (UserModel.findByPk as jest.Mock).mockResolvedValue(user);
+            await userController.deleteUser(req as Request, res as Response);
+            expect(UserModel.findByPk).toHaveBeenCalledWith('1');
+            expect(user.destroy).toHaveBeenCalled();
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: true,
+                error: false,
+                message: 'Success!',
+            });
+        });
+        it('deve retornar null se o usuário não for encontrado', async () => {
+            req.params = { id: '1' };
+            (UserModel.findByPk as jest.Mock).mockResolvedValue(null);
+            await userController.deleteUser(req as Request, res as Response);
+            expect(UserModel.findByPk).toHaveBeenCalledWith('1');
+            expect(jsonMock).toHaveBeenCalledWith({
+                data: null,
+                error: true,
+                message: 'Bad Request!',
+            });
+        });
+    });
+});
