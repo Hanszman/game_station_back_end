32439611c56f90bc1bf56ae18697c97abc20b1fa
Hanszman <victor.hanszman@hotmail.com>
2024-10-10 08:59:41 -0300

    updating data

--- src/database/scripts/dataInserts.sql
+++ src/database/scripts/dataInserts.sql
@@ -1,2 +1,3 @@
-INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('gamestation', 'gamestationdb', 'gamestation@gamestation.com', 'Game', 'Station');
-INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('test', 'test123', 'test@testing.com', 'test', NULL);
+INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('gamestation', '$2a$10$N5G5oydeEbnae8ZjIp1.XuuY7heV3DgBDHBcl0iTOlAK7TbbCE6fm', 'gamestation@gamestation.com', 'Game', 'Station');
+INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('test', '$2a$10$TV1g9bzBP.drjH5SNVSZPeFpbVkKd1.MexFR0LP2ojJ0BfDx6zq8y', 'test@testing.com', 'test', NULL);
+INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('hanszman', '$2a$10$Lu7IW5ZLQzjgPadjKF0dse/vxCRj1QKncZItcdYk.0hOHOLGpJveu', 'victor.hanszman@hotmail.com', 'Victor', 'Hanszman');
