048f27d48775aba009e365bb3b25dc7352a8d024
Hanszman <victor.hanszman@hotmail.com>
2024-10-27 14:12:40 -0300

    creating game table

--- src/database/scripts/dataInserts.sql
+++ src/database/scripts/dataInserts.sql
@@ -1,3 +1,8 @@
-INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('gamestation', '$2a$10$N5G5oydeEbnae8ZjIp1.XuuY7heV3DgBDHBcl0iTOlAK7TbbCE6fm', 'gamestation@gamestation.com', 'Game', 'Station');
-INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('test', '$2a$10$TV1g9bzBP.drjH5SNVSZPeFpbVkKd1.MexFR0LP2ojJ0BfDx6zq8y', 'test@testing.com', 'test', NULL);
-INSERT INTO gamestationdb.`user` (username, password, email, name, lastname) VALUES ('hanszman', '$2a$10$Lu7IW5ZLQzjgPadjKF0dse/vxCRj1QKncZItcdYk.0hOHOLGpJveu', 'victor.hanszman@hotmail.com', 'Victor', 'Hanszman');
+-- User:
+INSERT INTO gamestationdb.user (username, password, email, name, lastname) VALUES ('gamestation', '$2a$10$N5G5oydeEbnae8ZjIp1.XuuY7heV3DgBDHBcl0iTOlAK7TbbCE6fm', 'gamestation@gamestation.com', 'Game', 'Station');
+INSERT INTO gamestationdb.user (username, password, email, name, lastname) VALUES ('test', '$2a$10$TV1g9bzBP.drjH5SNVSZPeFpbVkKd1.MexFR0LP2ojJ0BfDx6zq8y', 'test@testing.com', 'test', NULL);
+INSERT INTO gamestationdb.user (username, password, email, name, lastname) VALUES ('hanszman', '$2a$10$Lu7IW5ZLQzjgPadjKF0dse/vxCRj1QKncZItcdYk.0hOHOLGpJveu', 'victor.hanszman@hotmail.com', 'Victor', 'Hanszman');
+INSERT INTO gamestationdb.user (username, password, email, name, lastname) VALUES ('rafa', '$2a$10$HhNWyvQ9vWCm6CyDH5EVO.SEP4mOfOlE1uDGiZD6IjbvlbrGHB3Si', 'rafa@gmail.com', 'Rafaela', 'Hanszman');
+
+-- Game:
+INSERT INTO gamestationdb.game (name, genre) VALUES ('TicTacToe', 'Puzzle');
--- src/database/scripts/schema.sql
+++ src/database/scripts/schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE `user` (
   `lastname` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`)
 )
+
+CREATE TABLE `game` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(300) NOT NULL,
+  `genre` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+)
+
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
