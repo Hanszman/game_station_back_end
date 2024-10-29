7475ae2a2d6872116b8b33737f46e0f0c2e23367
Hanszman <victor.hanszman@hotmail.com>
2024-10-29 08:32:44 -0300

    creating relational table for user_game

--- src/database/scripts/schema.sql
+++ src/database/scripts/schema.sql
@@ -15,4 +15,19 @@ CREATE TABLE `game` (
   PRIMARY KEY (`id`)
 )
 
+CREATE TABLE `user_game` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `game_id` int NOT NULL,
+  `wins` int DEFAULT NULL,
+  `losses` int DEFAULT NULL,
+  `draws` int DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_game_FK_user` (`user_id`),
+  KEY `user_game_FK_game` (`game_id`),
+  CONSTRAINT `user_game_FK_game` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+  CONSTRAINT `user_game_FK_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+)
+
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
