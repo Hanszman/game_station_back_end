4568566c1463cbaffbf60c9675ce21cbcaaa749e
Hanszman <victor.hanszman@hotmail.com>
2024-11-05 07:53:26 -0300

    implementing usergame routes

--- src/routes/apiRoutes.ts
+++ src/routes/apiRoutes.ts
@@ -3,6 +3,7 @@ import generalController from '../controllers/generalController';
 import loginController from '../controllers/loginController';
 import userController from '../controllers/userController';
 import gameController from '../controllers/gameController';
+import userGameController from '../controllers/userGameController';
 const router = express.Router();
 
 // GET
@@ -11,16 +12,21 @@ router.get('/user', userController.readUser);
 router.get('/user/:id', userController.readUser);
 router.get('/game', gameController.readGame);
 router.get('/game/:id', gameController.readGame);
+router.get('/usergame', userGameController.readUserGame);
+router.get('/usergame/:id', userGameController.readUserGame);
 router.get('/db', generalController.dbRoute);
 
 // POST
 router.post('/login', loginController.loginAuth);
 router.post('/user', userController.createUser);
+router.post('/usergame', userGameController.createUserGame);
 
 // PUT
 router.put('/user/:id', userController.updateUser);
+router.put('/usergame/:id', userGameController.updateUserGame);
 
 // DELETE
 router.delete('/user/:id', userController.deleteUser);
+router.delete('/usergame/:id', userGameController.deleteUserGame);
 
 export default router;
